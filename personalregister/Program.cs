a20df6c4c13f503bad96f00d130f8d3a525f06bb
Daniel Wallén <daniel.wallen@bahnhof.se>
2022-05-10 17:00:03 +0200

    Add project files.

---
+++ personalregister/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+/*
+Person Employee = new();
+
+
+Console.WriteLine("Input name:");
+Employee.Name = Console.ReadLine();
+
+Console.WriteLine("Input Pay:");
+Employee.Pay = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Employee.Name + " " + Employee.Pay);
+*/
+
+
+class Person
+{
+    public string Name = "";
+    public int Pay;
+
+}
+
+class Staff
+{
+    Person[] Employees = {};
+    public void AddStaff(string Name, int Pay)
+    {
+        Person Employee = new();
+        Employee.Name=Name;
+        Employee.Pay=Pay;
+        Employees.Append(Employee);
+    }
+    public void ListStaff()
+    {
+        for(int i=0;i<Employees.Count() ; i++)
+        {
+            Person Employee = Employees[i];
+            Console.WriteLine(Employee.Name + " " + Employee.Pay);
+        }
+    }
+}
